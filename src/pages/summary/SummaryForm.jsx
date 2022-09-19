fa23b9ca4845e33d95277a64b1254c0667a78fbd
Baktybek Tazhibaev <btazhibaev@coolerscreens.com>
2022-09-18 20:07:27 -1000

    completed unhover on summary form

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,23 +1,46 @@
 import { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Popover, OverlayTrigger } from 'react-bootstrap';
 
 const SummaryForm = () => {
   const [tcChecked, setTcChecked] = useState(false);
 
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
+
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: 'blue' }}> Terms and Conditions</span>
+      I agree to{' '}
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span
+          style={{
+            color: 'blue',
+          }}
+        >
+          Terms and Conditions
+        </span>
+      </OverlayTrigger>
     </span>
   );
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target),
+      formDataObj = Object.fromEntries(formData.entries());
+    console.log(formDataObj);
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group controlId="terms-and-conditions">
-        <Form.Check 
-            type="checkbox" 
-            label={checkboxLabel}
-            onChange={(e) => setTcChecked(e.target.checked)}
-            checked={tcChecked}
+        <Form.Check
+          name="culture"
+          type="checkbox"
+          label={checkboxLabel}
+          onChange={(e) => setTcChecked(e.target.checked)}
+          checked={tcChecked}
         />
       </Form.Group>
       <Button variant="primary" type="submit" disabled={!tcChecked}>
